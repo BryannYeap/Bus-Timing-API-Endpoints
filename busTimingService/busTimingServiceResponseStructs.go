bf821308790976997c3feb309e2462ae372b5130
BryannYeap <e0543723@u.nus.edu>
2022-03-14 08:21:19 +0800

    Add comments to clarify package structs

--- busTimingService/busTimingServiceResponseStructs.go
+++ busTimingService/busTimingServiceResponseStructs.go
@@ -1,25 +1,30 @@
 package busTimingService
 
+// Response to BusStopRequest
 type BusStop struct {
 	BusStop_ID int
 	Name string
 	BusForecasts []BusForecast `json:",omitempty"`
 }
 
+// Response to BusLineRequest
 type BusLineWithBuses struct {
 	BusLine BusLine
 	Buses []Bus
 }
 
+// Response to BusLineWithBusStopsRequest
 type BusLineWithBusStops struct {
 	BusLine BusLine
 	BusStops []BusStop
 }
 
+// Response to CurrentBusesRequest
 type CurrentBuses struct {
 	Buses []BusWithBusLines 
 }
 
+// Response to BusTimingRequest
 type BusTiming struct {
 	Bus Bus
 	BusLines []BusLineWithBusStops
